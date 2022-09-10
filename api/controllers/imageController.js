72c987ad6cbbdedd039219af7748b0e77a0b0d16
scorcuera <sc@thehotelsnetwork.com>
2022-09-10 21:03:43 +0200

    feat(controllers): define api controller methods

---
+++ api/controllers/imageController.js
@@ -0,0 +1,80 @@
+import { getConnection } from "../database/database";
+
+const getImages = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, name, url FROM apes-data");
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const getImage = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, name, url FROM apes-data WHERE id = ?", id);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const deleteImage = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM apes-data WHERE id = ?", id);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const addImage = async (req, res) => {
+    try {
+        const { body, file } = req;
+        const name = body.name;
+        const image_url = body.image_url || file.filename;
+        const image = {name, image_url};
+        const connection = await getConnection();
+        const result = await connection.query("INSERT INTO apes-data SET ?", image);
+        res.status(201);
+        if (res.status(201)) {
+            res.json(req.body);
+        }
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+const updateImage = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { body, file } = req;
+        const name = body.name;
+        const image_url = body.image_url || file.filename;
+        const image = {name, image_url};
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE apes-data SET ? WHERE id = ?", [image, id]);
+        if (res.status(200)) {
+            res.json(req.body);
+        }
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+export const methods = {
+    getImages,
+    addImage,
+    getImage,
+    updateImage,
+    deleteImage
+};
