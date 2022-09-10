25eb4b4ca19ce45dfc26784f4f3c608abdba4e70
scorcuera <sc@thehotelsnetwork.com>
2022-09-10 21:21:36 +0200

    fix(routes): update imageController methods

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { methods as languageController } from "../controllers/languageController";
+import { methods as imageController } from "../controllers/imageController";
 import { storage } from "../config/multer";
 import multer from "multer";
 const uploader = multer({storage});
 
 const router = Router();
 
-router.get("/", languageController.getLanguages);
-router.get("/:id", languageController.getLanguage);
-router.post("/", uploader.single('image_url'), languageController.addLanguage);
-router.put("/:id", uploader.single('image_url'), languageController.updateLanguage);
-router.delete("/:id", languageController.deleteLanguage);
+router.get("/", imageController.getImages);
+router.get("/:id", imageController.getImage);
+router.post("/", uploader.single('image_url'), imageController.addImage);
+router.put("/:id", uploader.single('image_url'), imageController.updateImage);
+router.delete("/:id", imageController.deleteImage);
 
 export default router;
